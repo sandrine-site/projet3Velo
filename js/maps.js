69062887518e1c97c52e5a94747db26822d0f376
Cloitre Sandrine <44755735+slashcreations@users.noreply.github.com>
2018-12-05 18:37:14 +0100

    Projet globaliement fini reste:
    Mettre quelques éléments en Poo+
    problème sur le décompteur de temps

--- js/maps.js
+++ js/maps.js
@@ -1,158 +1,157 @@
- //On definit l'apparence de la carte
- var mymap = L.map('mapid').setView([43.296867, 5.387092], 13);
- L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1Ijoic2FuZHJpbmVjbG9pdHJlIiwiYSI6ImNqcDJwZW5kMzA4dXgza3A4Nndxa2d3ZnoifQ.YmmEHgjVDPWedeomCY24cA', {
-   attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-   maxZoom: 18,
-   id: 'mapbox.streets',
-   accessToken: 'pk.eyJ1Ijoic2FuZHJpbmVjbG9pdHJlIiwiYSI6ImNqcDJwZW5kMzA4dXgza3A4Nndxa2d3ZnoifQ.YmmEHgjVDPWedeomCY24cA'
- }).addTo(mymap);
- var resa = 1;
- // définition des icones
- var myIconOrange = L.icon({
-   iconUrl: 'images/VeloOrange.png',
-   iconSize: [21, 24]
- });
- var myIconOrangeBonus = L.icon({
-   iconUrl: 'images/VeloOrangeBonus.png',
-   iconSize: [21, 24]
- });
- var myIconFermee = L.icon({
-   iconUrl: 'images/StationFermee.png',
-   iconSize: [21, 24]
- });
- var myIconZero = L.icon({
-   iconUrl: 'images/VeloZero.png',
-   iconSize: [21, 24]
- });
- var myIconLibreBonus = L.icon({
-   iconUrl: 'images/VeloLibreBonus.png',
-   iconSize: [21, 24]
- });
- var myIconLibre = L.icon({
-   iconUrl: 'images/VeloLibre.png',
-   iconSize: [21, 24]
- });
+var sessionStorage;
+//On definit l'apparence de la carte
+var mymap = L.map('mapid').setView([43.296867, 5.387092], 13);
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1Ijoic2FuZHJpbmVjbG9pdHJlIiwiYSI6ImNqcDJwZW5kMzA4dXgza3A4Nndxa2d3ZnoifQ.YmmEHgjVDPWedeomCY24cA', {
+  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+  maxZoom: 18,
+  id: 'mapbox.streets',
+  accessToken: 'pk.eyJ1Ijoic2FuZHJpbmVjbG9pdHJlIiwiYSI6ImNqcDJwZW5kMzA4dXgza3A4Nndxa2d3ZnoifQ.YmmEHgjVDPWedeomCY24cA'
+}).addTo(mymap);
+var resa = 1;
+// définition des icones
+var myIconOrange = L.icon({
+  iconUrl: 'images/VeloOrange.png',
+  iconSize: [21, 24]
+});
+var myIconOrangeBonus = L.icon({
+  iconUrl: 'images/VeloOrangeBonus.png',
+  iconSize: [21, 24]
+});
+var myIconFermee = L.icon({
+  iconUrl: 'images/StationFermee.png',
+  iconSize: [21, 24]
+});
+var myIconZero = L.icon({
+  iconUrl: 'images/VeloZero.png',
+  iconSize: [21, 24]
+});
+var myIconLibreBonus = L.icon({
+  iconUrl: 'images/VeloLibreBonus.png',
+  iconSize: [21, 24]
+});
+var myIconLibre = L.icon({
+  iconUrl: 'images/VeloLibre.png',
+  iconSize: [21, 24]
+});
 
- var markersCluster = new L.MarkerClusterGroup({
-   spiderfyOnMaxZoom: false,
-   showCoverageOnHover: false
- });
+var markersCluster = new L.MarkerClusterGroup({
+  spiderfyOnMaxZoom: false,
+  showCoverageOnHover: false
+});
 
- // mise en place des marqueurs stations
- var url = 'https://api.jcdecaux.com/vls/v1/stations?contract=marseille&apiKey=ad3a6e37420a676bfb271c60b981f4adf221ae89';
- ajaxGet(url, function (reponse) {
-   console.log(resa);
-   var stations = JSON.parse(reponse);
-   stations.forEach(function (station) {
-     if (station.status != "OPEN") {
+// mise en place des marqueurs stations
+var url = 'https://api.jcdecaux.com/vls/v1/stations?contract=marseille&apiKey=ad3a6e37420a676bfb271c60b981f4adf221ae89';
+ajaxGet(url, function (reponse) {
 
-       var addMarker = L.marker([station.position.lat, station.position.lng], {
-         icon: myIconFermee,
-         title: station.number
-       }).on('click', function () {
-         afficheForm(mymap, station);
-       })
-       markersCluster.addLayer(addMarker);
-     } else {
-       if (station.available_bikes === 0) {
+  var stations = JSON.parse(reponse);
+  stations.forEach(function (station) {
+    if (station.name === sessionStorage.getItem("stationName")) {
 
-         var addMarker = L.marker([station.position.lat, station.position.lng], {
-           icon: myIconZero,
-           title: station.number
-         }).on('click', function () {
-           afficheForm(mymap, station);
-         })
-         markersCluster.addLayer(addMarker);
-       } else if (station.available_bikes <= 5 && station.bonus === true) {
+      station.available_bikes = station.available_bikes - 1
+    };
+    if (station.status != "OPEN") {
+      var addMarker = L.marker([station.position.lat, station.position.lng], {
+        icon: myIconFermee,
+        title: station.number
+      }).on('click', function () {
+        afficheForm(mymap, station);
+      })
+      markersCluster.addLayer(addMarker);
+    } else {
+      if (station.available_bikes === 0) {
+        var addMarker = L.marker([station.position.lat, station.position.lng], {
+          icon: myIconZero,
+          title: station.number
+        }).on('click', function () {
+          afficheForm(mymap, station);
+        })
+        markersCluster.addLayer(addMarker);
+      } else if (station.available_bikes <= 5 && station.bonus === true) {
+        var addMarker = L.marker([station.position.lat, station.position.lng], {
+          icon: myIconOrangeBonus,
+          title: station.number
+        }).on('click', function () {
+          afficheForm(mymap, station);
+        })
+        markersCluster.addLayer(addMarker);
+      } else if (station.available_bikes <= 5 && station.bonus === false) {
+        resa = 1;
+        var addMarker = L.marker([station.position.lat, station.position.lng], {
+          icon: myIconOrange,
+          title: station.number
+        }).on('click', function () {
+          afficheForm(mymap, station);
+        })
+        markersCluster.addLayer(addMarker);
+      } else if (station.available_bikes > 5 && station.bonus === false) {
+        var addMarker = L.marker([station.position.lat, station.position.lng], {
+          icon: myIconLibre,
+          title: station.number
+        }).on('click', function () {
+          afficheForm(mymap, station);
+        })
+        markersCluster.addLayer(addMarker);
+      } else if (station.available_bikes > 5 && station.bonus === true) {
+        var addMarker = L.marker([station.position.lat, station.position.lng], {
+          icon: myIconLibreBonus,
+          title: station.number
+        }).on('click', function () {
+          afficheForm(mymap, station);
+        })
+        markersCluster.addLayer(addMarker);
+      }
+    }
 
 
-         var addMarker = L.marker([station.position.lat, station.position.lng], {
-           icon: myIconOrangeBonus,
-           title: station.number
-         }).on('click', function () {
-           afficheForm(mymap, station);
-         })
-         markersCluster.addLayer(addMarker);
-       } else if (station.available_bikes <= 5 && station.bonus === false) {
-         resa = 1;
-         var addMarker = L.marker([station.position.lat, station.position.lng], {
-           icon: myIconOrange,
-           title: station.number
-         }).on('click', function () {
-           afficheForm(mymap, station);
-         })
-         markersCluster.addLayer(addMarker);
-       } else if (station.available_bikes > 5 && station.bonus === false) {
+    //    Affichage de la partie description de la station et formulaire de reservation
 
-         var addMarker = L.marker([station.position.lat, station.position.lng], {
-           icon: myIconLibre,
-           title: station.number
-         }).on('click', function () {
-           afficheForm(mymap, station);
-         })
-         markersCluster.addLayer(addMarker);
-       } else if (station.available_bikes > 5 && station.bonus === true) {
+    function afficheForm(mymap, station) {
 
-         var addMarker = L.marker([station.position.lat, station.position.lng], {
-           icon: myIconLibreBonus,
-           title: station.number
-         }).on('click', function () {
-           afficheForm(mymap, station);
-         })
-         markersCluster.addLayer(addMarker);
-       }
-     }
-     //    Affichage de la partie description de la station et formulaire de reservation
-     function afficheForm(mymap, station) {
-       document.getElementById("fenetreStation").innerHTML = '';
-       document.getElementById("fenetreStation").innerHTML += '<h3> Détail de la station : ' + station.name + '</h3><p>Adresse : ' + station.address + '<br/><br/>' + station.bike_stands + ' places </li><br/>' + station.available_bikes + ' vélos disponibles </p>';
-       document.getElementById("fenetreStation").style.visibility = "visible";
-       document.getElementById("fenetrereservation").style.visibility = "visible";
+      document.getElementById("fenetreStation").innerHTML = '';
+      document.getElementById("fenetreStation").innerHTML += '<h3> Détail de la station : ' + station.name + '</h3><p>Adresse : ' + station.address + '<br/><br/>' + station.bike_stands + ' places </li><br/>' + station.available_bikes + ' vélos disponibles </p>';
 
-       if (station.status != "OPEN") {
-         document.getElementById("fenetreStation").innerHTML += "<h3>Désolé cette station est actuellement fermée<h3>";
+      // stokage des variables station.Name et station.available_bikes dans une SessionStorage.
+      sessionStorage.setItem("stationName", station.name);
+      sessionStorage.setItem("stationVelos", station.available_bikes);
 
-         document.getElementById("formulaire").style.visibility = "hidden";
-       } else {
+      document.getElementById("fenetreStation").style.visibility = "visible";
+      document.getElementById("fenetrereservation").style.visibility = "visible";
 
-         if (station.available_bikes !== 0) {
-           faireReservation()
-         } else if (station.available_bikes === 0) {
-           document.getElementById("fenetreStation").innerHTML += "<h3>Il n'y a pas de vélo disponible à cette station.<h3>";
-           resa = 1;
-           document.getElementById("formulaire").style.visibility = "hidden";
-         } else {
-           document.getElementById("fenetreStation").innerHTML += "<h3> Attention, vous avez déjà une réservation en cours, une nouvelle réservation entrainera une annulation de la précédente.<h3>";
+      if (station.status != "OPEN") {
+        document.getElementById("fenetreStation").innerHTML += "<h3>Désolé cette station est actuellement fermée<h3>";
 
-           faireReservation();
-         }
-       };
-     }
-   });
- });
+        document.getElementById("formulaire").style.visibility = "hidden";
+      } else {
 
- function faireReservation() {
-   document.getElementById("fenetrereservation").style.visibility = "visible";
-   document.getElementById("formulaire").style.visibility = "visible";
-   console.log(localStorage);
-   if (localStorage.length != 0) {
-     document.getElementById("nom").value = localStorage.getItem("nom");
-     document.getElementById("prenom").value = localStorage.getItem("prenom");
-   } else {
-     document.getElementById("nom").placeholder = '';
-     document.getElementById("prenom").placeholder = '';
-   }
-   var form = document.querySelector("form");
-   form.addEventListener("submit", function (e) {
-     var nom = form.elements.nom.value;
-     var prenom = form.elements.prenom.value;
-     console.log(nom);
-     //penser à rajouter une condition sur la signature
-     localStorage.setItem("nom", nom);
-     localStorage.setItem("prenom", prenom);
-     //     document.getElementById("presentationmessage").style.visibility = "visible";
-     //     document.getElementById("Message").innerHTML += "<h3> Attention, vous avez déjà une réservation en cours, une nouvelle réservation entrainera une annulation de la précédente.<h3>";
+        if (station.available_bikes !== 0) {
+          faireReservation()
+        } else if (station.available_bikes === 0) {
+          document.getElementById("fenetreStation").innerHTML += "<h3>Il n'y a pas de vélo disponible à cette station.<h3>";
+          resa = 1;
+          document.getElementById("formulaire").style.visibility = "hidden";
+        } else {
+          document.getElementById("fenetreStation").innerHTML += "<h3> Attention, vous avez déjà une réservation en cours, une nouvelle réservation entrainera une annulation de la précédente.<h3>";
+          faireReservation();
+        }
+      };
+    }
+  });
+});
 
-   });
- }
- mymap.addLayer(markersCluster);
+function faireReservation() {
+
+  document.getElementById("fenetrereservation").style.visibility = "visible";
+  document.getElementById("formulaire").style.visibility = "visible";
+
+  if (localStorage.length != 0) {
+    document.getElementById("nom").value = localStorage.getItem("nom");
+    document.getElementById("prenom").value = localStorage.getItem("prenom");
+  } else {
+    document.getElementById("nom").placeholder = '';
+    document.getElementById("prenom").placeholder = '';
+  }
+
+
+};
+
+mymap.addLayer(markersCluster);
--- js/signature.js
+++ js/signature.js
@@ -1,3 +1,10 @@
+
+
+
+
+var len;
+var localStorage;
+
 function init() {
 
   var body = document.getElementById("body");
@@ -5,70 +12,146 @@ function init() {
   var context = canvas.getContext("2d");
   var dessin = document.getElementById("dessin");
   var inputType = "mouse";
+  var stylo;
 
-  //on ecrit définit le trait
+
+  //on définit les carractéristiques du trait
   context.lineWidth = 1.5;
   context.lineCap = "round";
   context.lineJoin = "round";
   context.strokeStyle = "#3364fe";
 
   // on relie les évenements de la souris au dessin dans le canvas à l'aide d' EventListenner
-
   canvas.addEventListener("mousedown", function (e) {
-
     stylo = true;
-    addClick(e.layerX - canvas.offsetLeft, (e.layerY - canvas.offsetTop) / 2);
-    console.log(e.layerY - canvas.offsetTop + " = " + e.layerY + " - " + canvas.offsetTop);
-    console.log(e.layerX - canvas.offsetLeft + " = " + e.layerX + " - " + canvas.offsetLeft);
+    addClick(e.layerX - canvas.offsetLeft, ((e.layerY - canvas.offsetTop) / 2) + 15, false);
     dessiner();
+    e.preventDefault();
+    e.stopPropagation();
   });
 
-  //stylo = pointe du stylo si =true =click souris =>on ecrit
-  canvas.addEventListener("mouseup", function () {
-    stylo = false;
-  })
-
-  // si le stylo est appuyé et dans le canvas on dessine
+  // si le stylo est appuyé et dans le canvas on dessine (glissement)
   canvas.addEventListener("mousemove", function (e) {
     if (stylo === true) {
-      addClick(e.layerX - canvas.offsetLeft, (e.layerY - canvas.offsetTop) / 2);
-      console.log(e.layerY - canvas.offsetTop + " = " + e.layerY + " - " + canvas.offsetTop);
-      console.log(e.layerX - canvas.offsetLeft + " = " + e.layerX + " - " + canvas.offsetLeft);
-      dessiner();
+      addClick(e.layerX - canvas.offsetLeft, ((e.layerY - canvas.offsetTop) / 2) + 15, true);
+      dessiner()
     }
   });
 
+  //stylo = pointe du stylo si =true =click souris =>on ecrit
+  canvas.addEventListener("mouseup", function (e) {
+    stylo = false
+  })
+
   // si le stylo sort de la zonne canvas on arrete le dessin
   canvas.addEventListener("mouseleave", function () {
-    stylo = false;
+    stylo = false
   });
 
-  //enregistrement de la position de la sourisdans un tableau;
+
+  //enregistrement de la position de la sourisdans ainsi que du glissement un tableau;
   var clickX = new Array();
   var clickY = new Array();
   var clickDrag = new Array();
-  var stylo;
+  len = clickX.length;
 
   function addClick(x, y, dragging) {
     clickX.push(x);
     clickY.push(y);
     clickDrag.push(dragging);
+    len = clickX.length;
   }
+
   //à chaque appel de la fonction dessiner on efface le context et on redessine tout
+
   function dessiner() {
-    context.clearRect(0, 0, 300, 300);
+    context.clearRect(0, 0, context.canvas.width, context.canvas.height);
     for (var i = 0; i < clickX.length; i++) {
       context.beginPath();
       if (clickDrag[i] && i) {
         context.moveTo(clickX[i - 1], clickY[i - 1]);
       } else {
-        context.moveTo(clickX[i] - 1, clickY[i] - 1);
+        context.moveTo(clickX[i] - 1, clickY[i]);
       }
-      context.quadraticCurveTo(clickX[i], clickY[i], clickX[i + 1], clickY[i + 1]);
+      context.lineTo(clickX[i], clickY[i]);
       context.closePath();
       context.stroke();
     }
   }
 }
-
+//Lancement de la fonction signature (init) quand tous est prêt
 window.onload = init
+
+//Validation du formulaire et affichage du message de confirmation
+var compteur = 0;
+var form = document.querySelector("form");
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  var nomElt =  document.getElementById("nom");
+  var nom=form.elements.nom.value
+ var prenomElt =  document.getElementById("prenom");
+  var prenom=prenomElt.value
+console.log(nom);
+  ecrireMessage()
+
+
+//Validation du formulaire par l'utilisateur
+
+
+function ecrireMessage() {
+  // len=longueur de la liste des position de la souris pour écrire
+  if (len !== 0 && nom !== "" && prenom !== "") {
+var textetemps = ""
+    temps = 1200;
+   
+    // Si le formulaire est valide on stoke le nom et le prenom dans une variable locale 
+    localStorage.nom=nom;
+    localStorage.prenom=prenom;
+console.log(localStorage.nom);
+    document.getElementById("presentationMessage").style.visibility = "visible";
+
+
+    document.getElementById("message").innerHTML += "<h3>Vélo réservé à la station " + sessionStorage.getItem("stationName") + " par " + localStorage.getItem("prenom") + " " + localStorage.getItem("nom") +affichage(1200)+" </h3>"
+    console.log("<h3>Vélo réservé à la station " + sessionStorage.getItem("stationName") + " par " + localStorage.getItem("prenom") + " " + localStorage.getItem("nom") + affichage(1200) + " </h3>")
+
+  
+
+//décompte du temps qu'il reste
+    function affichage (tempsInitial)
+   
+{
+   diminuerCompteur(tempsInitial);
+  function diminuerCompteur(temps) {
+  if (temps > 0) {
+    temps = temps - 1
+     var tempsL=tempsLitteral(temps);
+    sout = setTimeout(diminuerCompteur, 1000);
+  } else {
+    clearInterval
+  }
+return tempsL
+}
+function tempsLitteral(seconde) {
+  var s, m
+  s = seconde % 60;
+  m = seconde >= 60 ? Math.floor(seconde / 60 % 60) : 0;
+  
+  if (m !== 0) {
+    if (s !== 0) {
+      textetemps = ", il vous reste : " + m + " min." + s + " s.</p>"
+    } else {
+      textetemps = "<p>, il vous reste : " + m + " min."
+    }
+  } else {
+    if (s !== 0) {
+      textetemps = ", il vous reste : " + s + " s."
+    } else {
+      textetemps = ", le temps est écoulé votre réservation a été annulée."
+    };
+  }
+  return textetemps
+}};}
+else if (len === 0){
+  document.getElementById("signature").style.border = " 2px solid red";
+}
+}});
